56d096468e9ed96b5a92bd913f907eedf9a7869e
Andre Sionek <andresionek91@gmail.com>
2018-12-03 23:00:36 -0200

    Update normalizer.py

--- normalizer.py
+++ normalizer.py
@@ -1,8 +1,7 @@
-# defining the min and max scores for normalization
-MIN_SCORE, MAX_SCORE = -8.054542604814351, 6.998829604417807
+from math import exp
 
 def normalize(score):
     """
-    Normalize to get values between 0 and 1000
+    Normalize to get values between 0 and 1000.
     """
-    return int((score - MIN_SCORE) / (MAX_SCORE - MIN_SCORE) * 1000)
+    return int(1 / (1 + exp(-score)) * 1000)
